f9d02fde1f6ad9cde8223f3a12a21b2a4eccc044
orynkanov <orynkanov@gmail.com>
2020-07-05 17:06:12 +0600

    fix show new line

--- terraform-proxmox-destroy.sh
+++ terraform-proxmox-destroy.sh
@@ -13,14 +13,14 @@ export TF_VAR_pm_host_sshuser='root'
 if [[ -z $TF_VAR_pm_password ]]; then
     read -r -s -p "Enter password for proxmox login $TF_VAR_pm_user: " PASS
     export TF_VAR_pm_password=$PASS
+    echo
 fi
-echo
 
 if [[ -z $TF_VAR_pm_host_sshpassword ]]; then
     read -r -s -p "Enter password for proxmox SSH host login $TF_VAR_pm_host_sshuser: " PASS
     export TF_VAR_pm_host_sshpassword=$PASS
+    echo
 fi
-echo
 
 cd "$SCRIPTDIR" || exit 1
 
