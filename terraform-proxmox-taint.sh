3f44900907d0bb077a06945cbef22b401c9069d1
orynkanov <orynkanov@gmail.com>
2020-07-06 23:56:35 +0600

    fix for test

--- terraform-proxmox-taint.sh
+++ terraform-proxmox-taint.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-#silent execution:
-#export TF_VAR_pm_password='PASSWORD'
-#export TF_VAR_pm_host_sshpassword='PASSWORD'
-
 SCRIPTDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-#start check
-if [[ ! -f $SCRIPTDIR/vars.cfg ]]; then
-    echo "File $SCRIPTDIR/vars.cfg not found"
+MODULE=$1
+
+if [[ ! $# -eq 1 ]]; then
+    echo "Usage - terraform-proxmox-taint.sh MODULE"
     exit 1
-fi
-#finish check
-
-# shellcheck source=/dev/null
-source "$SCRIPTDIR"/vars.cfg
-
-if [[ -z $TF_VAR_pm_password ]]; then
-    # shellcheck disable=SC2154
-    read -r -s -p "Enter password for proxmox login $TF_VAR_pm_user: " PMPASS
-    export TF_VAR_pm_password=$PMPASS
-    echo
-fi
-
-if [[ -z $TF_VAR_pm_host_sshpassword ]]; then
-    # shellcheck disable=SC2154
-    read -r -s -p "Enter password for proxmox SSH host login $TF_VAR_pm_host_sshuser: " SSHPASS
-    export TF_VAR_pm_host_sshpassword=$SSHPASS
-    echo
 fi
 
 cd "$SCRIPTDIR" || exit 1
@@ -41,18 +20,7 @@ if ! terraform validate; then
     echo "terraform validate failed"
     exit 1
 fi
-if ! terraform destroy; then
-    echo "terraform destroy failed"
-    exit 1
-else
-    echo "terraform destroy OK"
-fi
 
-#----------------------
-terraform show | grep module.salt | tr -d ':' | sed -e 's/module.salt.//'
-
-module=$1
-
-for resource in `terraform show -module-depth=1 | grep module.${module} | tr -d ':' | sed -e 's/module.${module}.//'`; do
-  terraform taint -module ${module} ${resource}
+for TAINT in $(terraform show | grep module."$MODULE" | tr -d ':'); do
+  terraform taint "$TAINT"
 done
