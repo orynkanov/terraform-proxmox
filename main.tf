3289a1f28ae6210fcf94955bb6759bc929eede6f
orynkanov <orynkanov@gmail.com>
2020-07-23 17:32:48 +0600

    test fix

--- main.tf
+++ main.tf
@@ -31,37 +31,37 @@ module "salt" {
   ]
 }
 
-module "ansible" {
-  source = "./vm"
-  pm_node = var.pm_node
-  vmtemplate = "tmplcentos8v1"
-  host = "ansible"
-  ip = "192.168.0.112"
-  onboot = false
-  sockets = 1
-  cores = 1
-  memory = 1024
-  disksize = 10
-  diskreplicate = true
-  pm_host_sshuser = var.pm_host_sshuser
-  pm_host_sshpassword = var.pm_host_sshpassword
-}
+# module "ansible" {
+#   source = "./vm"
+#   pm_node = var.pm_node
+#   vmtemplate = "tmplcentos8v1"
+#   host = "ansible"
+#   ip = "192.168.0.112"
+#   onboot = false
+#   sockets = 1
+#   cores = 1
+#   memory = 1024
+#   disksize = 10
+#   diskreplicate = true
+#   pm_host_sshuser = var.pm_host_sshuser
+#   pm_host_sshpassword = var.pm_host_sshpassword
+# }
 
-module "gitlab" {
-  source = "./vm"
-  pm_node = var.pm_node
-  vmtemplate = "tmplcentos7v1"
-  host = "gitlab"
-  ip = "192.168.0.113"
-  onboot = false
-  sockets = 1
-  cores = 2
-  memory = 4096
-  disksize = 10
-  diskreplicate = true
-  pm_host_sshuser = var.pm_host_sshuser
-  pm_host_sshpassword = var.pm_host_sshpassword
-}
+# module "gitlab" {
+#   source = "./vm"
+#   pm_node = var.pm_node
+#   vmtemplate = "tmplcentos7v1"
+#   host = "gitlab"
+#   ip = "192.168.0.113"
+#   onboot = false
+#   sockets = 1
+#   cores = 2
+#   memory = 4096
+#   disksize = 10
+#   diskreplicate = true
+#   pm_host_sshuser = var.pm_host_sshuser
+#   pm_host_sshpassword = var.pm_host_sshpassword
+# }
 
 module "ipa01" {
   source = "./vm"
